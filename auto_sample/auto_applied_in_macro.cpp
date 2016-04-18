d66f135625da537c75b7de676218e2c52e1a0ba3
githubch <944214536@qq.com>
2016-04-18 22:25:46 +0800

    [hcheng] 201604182225 combinet lamda with auto to implement max in macro

--- auto_sample/auto_applied_in_macro.cpp
+++ auto_sample/auto_applied_in_macro.cpp
@@ -11,6 +11,10 @@ int main(int argc, char ** argv){
     int b = 3;
     std::cout <<"Max(a, b)="<<Max(a,b)<<std::endl;
     std::cout <<"3 * Max(a, b) should equal 9, and the result is "<<3*Max(a, b)<<std::endl;
+    auto result = [=](){ return 4 * Max(a, b);};
+    std::cout<<result()<<std::endl;
+    std::cout <<"4 * Max(2, 3) should equal 12, and the result is "<<[=](){return 4*Max(a,b);}()<<std::endl;
+    std::cout <<"5 * Max(2, 3) should equal 15, and the result is "<<[=]()->float{return 5*Max(a, b)*1.0;}()<<std::endl;
     int m = 2;
     int n = 3;
     std::cout <<"Max_(m, n)="<<Max_(m++, n++)<<std::endl;
