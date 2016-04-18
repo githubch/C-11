59bd976c3c7848a33c7253a1cc25aaecd6de77bf
githubch <944214536@qq.com>
2016-04-18 22:14:00 +0800

    [hcheng] #201604182213 use auto to implement max in better performance for macro

--- auto_sample/auto_applied_in_macro.cpp
+++ auto_sample/auto_applied_in_macro.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
-#define Max(a,b) ((a > b) ? (a) : (b))
-#define Max_(a,b) {\
-        auto _a = a;\
-	auto _b = b;\ 
-	return (_a > _b) ? _a:_b;}\
+#define Max(a,b) ((a) > (b) ? (a) : (b))
+#define Max_(a,b) ({ \
+        auto _a = (a); \
+	auto _b = (b); \
+	(_a > _b) ? _a:_b;})\
 
  
 int main(int argc, char ** argv){
     int a = 2;
     int b = 3;
-    std::cout <<"Max(a, b)"<<Max(a,b)<<std::endl;
+    std::cout <<"Max(a, b)="<<Max(a,b)<<std::endl;
+    std::cout <<"3 * Max(a, b) should equal 9, and the result is "<<3*Max(a, b)<<std::endl;
     int m = 2;
     int n = 3;
-    std::cout <<"Max_(m, n)"<<Max_(m++, n++)<<std::endl;
+    std::cout <<"Max_(m, n)="<<Max_(m++, n++)<<std::endl;
+    std::cout <<"m="<<m<<std::endl;
+    std::cout <<"n="<<n<<std::endl;
 }
